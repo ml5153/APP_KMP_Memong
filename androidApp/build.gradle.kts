9020453c4c41ba0dcee8e208939eb113f1411be1
yspark <youngsue5153@gmail.com>
2025-11-23 15:18:00 +0900

    develop Change Structure{ Semantic Version } Current-Version { 1.0.0-2 }

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.memong.aos"
         minSdk = 21
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.0"
     }
     buildFeatures {
         buildConfig = true
